3b71b274435ab534b881349d70ea9c3cc08c42c8
tronghghe172557 <hoanggiatrang01@gmail.com>
2024-08-15 15:02:34 +0700

    feat: Kiểm tra hệ thống quá tải khi connect DB

--- src/app.js
+++ src/app.js
@@ -11,8 +11,8 @@ app.use(compression()) // nén băng thông
 
 // init db
 require('./dbs/init.mongodb.lvPro')
-const { countConnect } = require('./helpers/check.connect')
-countConnect();
+const { checkOverload } = require('./helpers/check.connect')
+// checkOverload();
 // init routes
 app.get('/', (req, res, next) => {
     return res.status(200).json({
--- src/helpers/check.connect.js
+++ src/helpers/check.connect.js
@@ -1,10 +1,34 @@
 const mongoose = require("mongoose");
+const os = require("os");
+const process = require("process");
+const _SECONDS = 5000; // Những tham số phải để const
 
 const countConnect = () => {
   const numConnection = mongoose.connections.length;
   console.log(`Number of connection::${numConnection}`);
 };
 
+// check overload
+const checkOverload = () => {
+  setInterval(() => {
+
+    const numConnection = mongoose.connections.length;
+    const numCores = os.cpus().length;
+    const memoryUsage = process.memoryUsage().rss;
+    // Example maximum number of connections based on number of cores
+    const maxConnection = numCores * 5;
+
+    console.log(`Active connections:: ${numConnection}`)
+    console.log(`Memory usage::${memoryUsage / 1024 / 1024} MB`)
+
+    if (numConnection > maxConnection) {
+      console.log(`Connection overload detected`);
+    }
+
+  }, _SECONDS); // Monitor every 5 seconds
+};
+
 module.exports = {
   countConnect,
+  checkOverload
 };
