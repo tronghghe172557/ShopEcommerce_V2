73a294ed70237c81242c8e8eab5bec5ddf5ffcbf
GithubCuaGiaTrong <hoanggiatrang01@gmail.com>
2024-10-28 09:11:52 +0700

    build: Config folder and file .bru

--- sever.js
+++ sever.js
@@ -3,9 +3,9 @@ const app = require('./src/app')
 const PORT = process.env.PORT || 3056
 
 const server = app.listen(PORT, () => {
-    console.log(`WSV eCommerce start with ${PORT}`)
+    console.log(`WSV eCommerce start with ${PORT}`, `localhost:${PORT}` )
 })
 
-process.on('SIGINT', () => {
-    server.close(() => console.log(`Exit Server Express`))
-})
+// process.on('SIGINT', () => {
+//     server.close(() => console.log(`Exit Server Express`))
+// })
