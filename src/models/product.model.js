2d24861d5aff313fe4f797c247349eedea892df0
tronghghe172557 <hoanggiatrang01@gmail.com>
2024-11-04 02:29:36 +0700

    feat: Create Product and attribute of product model

---
+++ src/models/product.model.js
@@ -0,0 +1,86 @@
+const mongoose = require("mongoose"); // Erase if already required
+
+const DOCUMENT_NAME = "Product";
+const COLLECTION_NAME = "Products";
+
+// Declare the Schema of the Mongo model
+const productSchema = new mongoose.Schema(
+  {
+    product_name: {
+      type: String,
+      required: true,
+    },
+    product_thumbnail: {
+      type: String,
+      required: true,
+    },
+    product_description: {
+      type: String,
+    },
+    product_price: {
+      type: String,
+      required: true,
+    },
+    product_quantity: {
+      type: Number,
+      required: true,
+    },
+    product_type: {
+      type: String,
+      required: true,
+      enum: ["Clothing", "Electronics", "Furniture"],
+    },
+    product_shop: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Shop",
+    },
+    product_attribute: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+// define the product type
+const clothingSchema = new mongoose.Schema({
+  brand: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+  },
+  material: {
+    type: String,
+  },
+}, {
+    collection: "Clothings",
+    timestamps: true,
+});
+
+const electronicSchema = new mongoose.Schema({
+  manufacturer: {
+    type: String,
+    required: true,
+  },
+  model: {
+    type: String,
+  },
+  color: {
+    type: String,
+  },
+}, {
+    collection: "Electronics",
+    timestamps: true,
+});
+
+//Export the model
+module.exports = {
+    product: mongoose.model(DOCUMENT_NAME, productSchema),
+    clothing: mongoose.model("Clothing", clothingSchema),
+    electronic: mongoose.model("Electronic", electronicSchema),
+}
